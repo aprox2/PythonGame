6b4f2253ba83adbfba89eafb861277a4253b0c17
aprox2 <edge15831@gmail.com>
2019-10-02 02:22:54 +0300

    Version 1.1 now even has pygame in it

---
+++ Arena/buttons.py
@@ -0,0 +1,36 @@
+import pygame as p
+
+
+class Buttons:
+    def __init__(self):
+        self.name = "Fallback"
+        self.fallBack = "assets/errorIMG.png"
+        self.buttDOWN = None
+        self.buttUP = None
+        self.xPos = -1
+        self.yPos = -1
+
+    def getNotPressed(self):
+        if self.buttUP == None:
+            return p.image.load(self.fallBack)
+        else:
+            return self.buttUP
+
+    def getPressed(self):
+        if self.buttDOWN == None:
+            return p.image.load(self.fallBack)
+        else:
+            return self.buttDOWN
+
+    def getPos(self):
+        if self.xPos == -1 or self.yPos == -1:
+            print("image is outside the screen, image name == \"{}\"".format(self.name))
+            return [-1, -1]
+        else:
+            return self.xPos, self.yPos
+
+    def getX(self):
+        return int(self.xPos)
+
+    def getY(self):
+        return int(self.yPos)
